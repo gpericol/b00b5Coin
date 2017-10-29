7438da900f38ae9df75a73d62cf052a1a3758e78
Gianluca Pericoli <gianluca.pericoli@INTERLOGICA.LAN>
2017-10-29 22:06:49 +0100

    refactor schemas

--- models/transaction.js
+++ models/transaction.js
@@ -3,120 +3,24 @@
 let Ajv = require('ajv');
 const Crypto = require('../utils/crypto-utils');
 
-const paymentSchema = {
-    "$schema": "http://json-schema.org/draft-06/schema#",
-    "title": "Payment",
-    "description": "a payment transaction",
-    "type": "object",
-    "properties": {
-        "tx": {
-            "type": "object",
-            "properties": {
-                "id": {
-                    "type": "string",
-                    "pattern": "^[0-9a-f]{64}$"
-                },
-                "from": {
-                    "type": "array",
-                    "items": {
-                        "type": "string"
-                    },
-                    "minItems": 1,
-                    "uniqueItems": true
-                },
-                "to": {
-                    "type": "array",
-                    "items": {
-                        "type": "object",
-                        "properties": {
-                            "address":{
-                                "type": "string"
-                            },
-                            "amount": {
-                                "type": "integer",
-                                "exclusiveMinimum": 0
-                            }
-                        },
-                        "required": ["address", "amount"]
-                    },
-                    "minItems": 1,
-                },
-                "msg": {
-                    "type": "string",
-                    "maxLength": 254
-                },
-                "fee": {
-                    "type": "integer",
-                    "exclusiveMinimum": 0
-                }
-            },
-            "required": ["id", "from", "to", "msg", "fee"]
-        },
-        "sign": {
-            "type": "array",
-            "items": {
-                "type": "string"
-            },
-            "minItems": 1,
-            "uniqueItems": true
-        }
-    },
-    "required" : ["tx", "sign"]
-}
-
-const rewardSchema = {
-    "$schema": "http://json-schema.org/draft-06/schema#",
-    "title": "Reward",
-    "description": "a reward transaction",
-    "type": "object",
-    "properties": {
-        "tx": {
-            "type": "object",
-            "properties": {
-                "id": {
-                    "type": "string",
-                    "pattern": "^[0-9a-f]{64}$"
-                },
-                "to": {
-                    "type": "object",
-                    "properties": {
-                        "address":{
-                            "type": "string"
-                        },
-                        "amount": {
-                            "type": "integer",
-                            "exclusiveMinimum": 0
-                        }
-                    },
-                    "required": ["address", "amount"]
-                },
-                "msg": {
-                    "type": "string",
-                    "maxLength": 254
-                }
-            },
-            "required": ["id", "to", "msg"]
-        },
-        "sign": {
-            "type": "string"
-        }
-    },
-    "required" : ["tx", "sign"]
-}
-
+const paymentSchema = require('./schemas/payment.json');
+const rewardSchema = require('./schemas/reward.json');
 
 class Transaction{
+    constructor(){
+        this.ajv = new Ajv();
+        this.validatePayment = this.ajv.compile(paymentSchema);
+        this.validateReward = this.ajv.compile(rewardSchema);
+    }
+
     /**
      * validateTransaction() validates a new transaction using schema, counting signatures and checking them
      * @param {*} transaction 
      * @return {boolean} validation
      */
     validateTransaction(transaction){
-        let ajv = new Ajv();
-        let validate = ajv.compile(paymentSchema);
-
         // validate with schema
-        if (!validate(transaction)){
+        if (!this.validatePayment(transaction)){
             return false;
         }
 
@@ -143,11 +47,8 @@ class Transaction{
      * @return {boolean} validation
      */
     validateReward(transaction){
-        let ajv = new Ajv();
-        let validate = ajv.compile(rewardSchema);
-
         // validate with schema
-        if (!validate(transaction)){
+        if (!validateReward(transaction)){
             return false;
         }
         
